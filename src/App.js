0cf072c087a775afbe48b26bd42d3b327aa8e653
bonyncloude <wnscjf3062@naver.com>
2023-03-24 12:03:49 +0900

    아침 공부 2

--- src/App.js
+++ src/App.js
@@ -1,22 +1,6 @@
 import './App.css';
-function Header(props){
-  console.log('pros',props,props.title)
-  return <header>
-    <h1><a href="/">{props.title}</a></h1>
-  </header>
-}
-function Nav(props){
-  const lis = []
-  for(let i=0; i<props.topics.length;i++){
-    let t=props.topics[i];
-    lis.push(<li key={t.id}><a href={'/read/'+t.id}>{t.title}</a></li>)
-  }
-  return<nav>
-        <ol>
-          {lis}
-        </ol>
-      </nav>
-}
+
+
 function Article(props){
   return <article>
     <h2>{props.title}</h2>
@@ -30,10 +14,38 @@ function App() {
   {id:3,title:'js',body:'js is ...'}
   
 ]
+function Header(props){
+  return <header>
+    <h1><a href="/" onClick={(event)=>{
+      event.preventDefault();
+      props.onChangeMode();
+    }}>{props.title}</a></h1>
+  </header>
+}
+function Nav(props){
+  const lis = []
+  for(let i=0; i<props.topics.length;i++){
+    let t=props.topics[i];
+    lis.push(<li key={t.id}>
+      <a id={t.id} href={'/read/'+t.id} onClick={event=>{
+        event.preventDefault();
+        props.onChangeMode(event.target.id);
+      }}>{t.title}</a></li>)
+  }
+  return<nav>
+        <ol>
+          {lis}
+        </ol>
+      </nav>
+}
   return (
     <div className="App">
-      <Header title="Welcome"></Header>
-      <Nav topics={topics}></Nav>
+      <Header title="Welcome" onChangeMode={()=>{
+        alert('Header');
+      }}></Header>
+      <Nav topics={topics} onChangeMode={(id)=>{
+        alert(id);
+      }}></Nav>
       <Article title="Welcome" body="Hello, WEB"></Article>
     </div>
   );
